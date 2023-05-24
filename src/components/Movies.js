9f2a803a8ede52de1127b678666f8233dbdab802
malcomowilla <owillamalcom@gmail.com>
2023-05-24 11:36:05 +0300

    solved

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,27 +1,58 @@
 import React from "react";
 import { movies } from "../data";
 
+
+
+
 function Movies() {
-  const moviesDisplay = movies.map((movie) => (
-    <div key={movie.title}>
-      <h3>Name: {movie.title}</h3>
-      <p>Time: {movie.time}</p>
-      Genres:
-      <ul>
-        {movie.genres.map((genre) => (
-          <li key={genre}>{genre}</li>
-        ))}
+ const moviesList = movies.map((movie, index)=>{
+  return (
+    <div key={index} >
+      <h1> movie title:{movie.title}</h1>
+      <h2> movie time: {movie.time} minutes</h2>
+      genre: <ul>{movie.genres.map((genre, index)=>{
+     return   <li key={`${index}-${genre}`}>{genre}</li>
+      })}
       </ul>
     </div>
-  ));
+  )
+ })
+
 
   return (
     <div>
-      {/*{code here}*/}
-      <h1>Movies Page</h1>
-      {moviesDisplay}
-    </div>
-  );
-}
+<h1> Movie Page</h1>
+{moviesList}
 
-export default Movies;
+    </div>
+  )
+
+
+
+
+}
+export default Movies
+// function Movies() {
+//   const moviesDisplay = movies.map((movie) => (
+//     <div key={movie.title}>
+//       <h3>Name: {movie.title}</h3>
+//       <p>Time: {movie.time}</p>
+//       Genres:
+//       <ul>
+//         {movie.genres.map((genre) => (
+//           <li key={genre}>{genre}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   ));
+
+//   return (
+//     <div>
+//       {/*{code here}*/}
+//       <h1>Movies Page</h1>
+//       {moviesDisplay}
+//     </div>
+//   );
+// }
+
+// export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,20 +3,30 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+  display:"inline-block",
+  width: "50px",
+  padding: "12px",
+  margin : "0 6px 6px",
+  background : "blue",
+  textDecoration: "none",
+  color: "white"
+}
+
 function NavBar() {
   return (
     <div className="navbar">
       {/*{code here}*/}
-      <NavLink to="/" exact>
+      <NavLink to="/" exact style={linkStyles}>
         Home
       </NavLink>
-      <NavLink to="/movies" exact>
+      <NavLink to="/movies" exact style={linkStyles}>
         Movies
       </NavLink>
-      <NavLink to="/directors" exact>
+      <NavLink to="/directors" exact style={linkStyles}>
         Directors
       </NavLink>
-      <NavLink to="/actors" exact>
+      <NavLink to="/actors" exact style={linkStyles}>
         Actors
       </NavLink>
     </div>
